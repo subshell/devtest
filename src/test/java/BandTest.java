91152c727fed1065d0293c5f98d39f7c1e56dc14
Konrad Schergaut <schergaut@subshell.com>
2020-07-28 13:31:25 +0200

    workspace

---
+++ src/test/java/BandTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Test;
+
+public class BandTest {
+
+    @Test
+    public void testIrgendwas() {
+        assertTrue(StringUtils.equalsIgnoreCase("foo", "bar"));
+    }
+
+}
